a51c24b7b0f6d142d551cdadbba0c0b8e7f4ce79
Donirvin Dickerson <Dond2018@gmail.com>
2019-06-17 11:45:10 -0700

    Updating game script with workable buttons and calculations, and fixing index file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,35 @@
-var win= 0;
-var lose= 0;
-var numRand = Math.floor(Math.random() * 1000) + 12;
-
 $(document).ready(function () {
-});
+    var win = 0;
+    var lose = 0;
+    var playerTotal = 0;
+
+    $('#getWins').text(win);
+    $('#getLosses').text(lose);
+
+    var randNum = Math.floor(Math.random() * 9999 + 12);
+
+    $('#randomNum').text(randNum);
+
+    var stone1 = Math.floor(Math.random() * 11 + 1);
+    var stone2 = Math.floor(Math.random() * 11 + 1);
+    var stone3 = Math.floor(Math.random() * 11 + 1);
+    var stone4 = Math.floor(Math.random() * 11 + 1);
+
+    $('#red').on('click', function () {
+        playerTotal = playerTotal + stone1;
+        $('#finalTotal').text(playerTotal); 
+         
+    })
+    $('#blue').on('click', function () {
+        playerTotal = playerTotal + stone2;
+        $('#finalTotal').text(playerTotal);
+    })
+    $('#purple').on('click', function () {
+        playerTotal = playerTotal + stone3;
+        $('#finalTotal').text(playerTotal);
+    })
+    $('#green').on('click', function () {
+        playerTotal = playerTotal + stone4;
+        $('#finalTotal').text(playerTotal);
+    })
+})

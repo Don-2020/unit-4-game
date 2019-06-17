1c94b6addadec10f039f940aea58c8d8ab838475
Donirvin Dickerson <Dond2018@gmail.com>
2019-06-17 12:16:16 -0700

    Updating the game.js and cleaning up index and css file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
     var win = 0;
-    var lose = 0;
+    var loss = 0;
     var playerTotal = 0;
 
     $('#getWins').text(win);
-    $('#getLosses').text(lose);
+    $('#getLosses').text(loss);
+    $('#finalTotal').text(playerTotal);
 
-    var randNum = Math.floor(Math.random() * 9999 + 12);
+    var randNum = Math.floor(Math.random() * 119 + 12);
 
     $('#randomNum').text(randNum);
 
@@ -15,21 +16,58 @@ $(document).ready(function () {
     var stone3 = Math.floor(Math.random() * 11 + 1);
     var stone4 = Math.floor(Math.random() * 11 + 1);
 
+    function Winner() {
+        alert("You won! You beat Thanos!!!");
+        win++;
+        $('#getWins').text(win);
+        reset();
+    }
+    function Loser() {
+        alert("YOU LOSS!!! Now Thanos has snap away half the population!!");
+        loss++;
+        $('#getLosses').text(loss);
+        reset();
+    }
+    function reset() {
+        playerTotal = 0;
+        stone1 = Math.floor(Math.random() * 11 + 1);
+        stone2 = Math.floor(Math.random() * 11 + 1);
+        stone3 = Math.floor(Math.random() * 11 + 1);
+        stone4 = Math.floor(Math.random() * 11 + 1);
+        randNum = Math.floor(Math.random() * 119 + 12);
+    }
+    function endGame() {
+        if (playerTotal === randNum) {
+            Winner();
+            reset();
+        }
+        else if (playerTotal > randNum) {
+            Loser();
+            reset();
+        }
+    }
+
+
     $('#red').on('click', function () {
         playerTotal = playerTotal + stone1;
-        $('#finalTotal').text(playerTotal); 
-         
+        $('#finalTotal').text(playerTotal);
+        endGame();
+
+
     })
     $('#blue').on('click', function () {
         playerTotal = playerTotal + stone2;
         $('#finalTotal').text(playerTotal);
+        endGame();
     })
     $('#purple').on('click', function () {
         playerTotal = playerTotal + stone3;
         $('#finalTotal').text(playerTotal);
+        endGame();
     })
     $('#green').on('click', function () {
         playerTotal = playerTotal + stone4;
         $('#finalTotal').text(playerTotal);
+        endGame();
     })
 })
